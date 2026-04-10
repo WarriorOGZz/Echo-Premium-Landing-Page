3f1409d4d42568295a42513278d784226a01e59a
WarriorOGZz <ujjwalsinghchauhan2011@gmail.com>
2026-04-10 18:29:50 +0530

    Implement dynamic letter positioning on mouse move
    Added mouse move functionality to dynamically position letters based on cursor location.

--- src/components/FlowerDecoration.tsx
+++ src/components/FlowerDecoration.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
+
 export default function FlowerDecoration() {
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+
+  const handleMove = (e: any) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = (e.clientX - rect.left) / rect.width - 0.5;
+    const y = (e.clientY - rect.top) / rect.height - 0.5;
+    setPos({ x, y });
+  };
+
   return (
     <div
+      onMouseMove={handleMove}
       style={{
         width: "100%",
         height: "100%",
@@ -19,86 +31,92 @@ export default function FlowerDecoration() {
           filter: "blur(70px)",
           top: "50%",
           left: "50%",
-          transform: "translate(-50%, -50%)",
+          transform: `translate(-50%, -50%) translate(${pos.x * 20}px, ${pos.y * 20}px)`,
         }}
       />
 
       {/* Letters */}
-      <div style={{ position: "relative", width: "100%", height: "100%" }}>
+      <Letter style={{
+        top: "20%",
+        left: "15%",
+        rotate: -12,
+        delay: 0,
+        pos
+      }}>E</Letter>
 
-        {/* E */}
-        <span className="float1 letter">E</span>
+      <Letter style={{
+        top: "35%",
+        left: "35%",
+        rotate: 8,
+        delay: 0.5,
+        pos
+      }}>C</Letter>
 
-        {/* C */}
-        <span className="float2 letter">C</span>
+      <Letter style={{
+        top: "55%",
+        left: "55%",
+        rotate: -6,
+        delay: 1,
+        pos
+      }}>H</Letter>
 
-        {/* H */}
-        <span className="float3 letter">H</span>
+      <Letter style={{
+        top: "30%",
+        left: "60%",
+        rotate: 10,
+        delay: 1.5,
+        pos
+      }}>O</Letter>
 
-        {/* O */}
-        <span className="float4 letter">O</span>
+      {/* TM */}
+      <span
+        style={{
+          position: "absolute",
+          top: "28%",
+          left: "75%",
+          color: "white",
+          fontSize: "14px",
+          opacity: 0.7,
+        }}
+      >
+        ™
+      </span>
+    </div>
+  );
+}
 
-        {/* TM */}
-        <span className="tm">™</span>
+function Letter({ children, style }: any) {
+  const { top, left, rotate, delay, pos } = style;
 
-      </div>
+  return (
+    <span
+      style={{
+        position: "absolute",
+        top,
+        left,
+        color: "white",
+        fontSize: "48px",
+        fontFamily: "serif",
+        opacity: 0.9,
+        transform: `
+          translate(${pos.x * 15}px, ${pos.y * 15}px)
+          rotate(${rotate}deg)
+        `,
+        animation: `floatY 4s ease-in-out ${delay}s infinite`,
+      }}
+    >
+      {children}
 
-      {/* Styles */}
       <style>{`
-        .letter {
-          position: absolute;
-          color: white;
-          font-size: 48px;
-          font-family: serif;
-          opacity: 0.9;
-        }
-
-        .float1 {
-          top: 20%;
-          left: 15%;
-          transform: rotate(-12deg);
-          animation: floatY 4s ease-in-out infinite;
-        }
-
-        .float2 {
-          top: 35%;
-          left: 35%;
-          transform: rotate(8deg);
-          animation: floatY 5s ease-in-out infinite;
-        }
-
-        .float3 {
-          top: 55%;
-          left: 55%;
-          transform: rotate(-6deg);
-          animation: floatY 4.5s ease-in-out infinite;
-        }
-
-        .float4 {
-          top: 30%;
-          left: 60%;
-          transform: rotate(10deg);
-          animation: floatY 6s ease-in-out infinite;
-        }
-
-        .tm {
-          position: absolute;
-          top: 28%;
-          left: 75%;
-          color: white;
-          font-size: 14px;
-          opacity: 0.7;
-        }
-
         @keyframes floatY {
           0%, 100% {
-            transform: translateY(0px) rotate(var(--rot, 0deg));
+            transform: translateY(0px) rotate(${rotate}deg);
           }
           50% {
-            transform: translateY(-12px) rotate(var(--rot, 0deg));
+            transform: translateY(-12px) rotate(${rotate}deg);
           }
         }
       `}</style>
-    </div>
+    </span>
   );
 }
